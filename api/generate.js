9c874112514bf1d6e526b561025f6d43fff4ff54
Eryüksel Aşkar <eryukselaskar@gmail.com>
2025-05-08 12:40:13 +0300

    Fix: SVG xmlns conflict and correct logo embedding

--- api/generate.js
+++ api/generate.js
@@ -18,27 +18,24 @@ export default async function handler(req, res) {
     if (format === 'svg') {
       let svg = await QRCode.toString(url, { type: 'svg', ...options });
 
-      // <svg> tag'ına xlink namespace ekle
-      svg = svg.replace(
-        '<svg',
-        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"'
-      );
-
-      // Logo varsa göm
       if (logoBase64) {
-        const logoSvg = `
-          <image href="${logoBase64}" x="35%" y="35%" height="30%" width="30%" />
-        `;
-        svg = svg.replace('</svg>', `${logoSvg}</svg>`);
+        const escapedLogo = logoBase64.replace(/"/g, '&quot;');
+
+        // SVG'yi aç, içine logo ekle ama <svg ...> tagine dokunma
+        const indexOfSvgClose = svg.indexOf('</svg>');
+        const logoSvg = `<image href="${escapedLogo}" x="35%" y="35%" width="30%" height="30%" />`;
+
+        // Yeni içerikli SVG üret
+        svg = svg.slice(0, indexOfSvgClose) + logoSvg + svg.slice(indexOfSvgClose);
       }
 
-      res.setHeader('Content-Type', 'image/svg+xml');
+      res.setHeader('Content-Type', 'image/svg+xml; charset=utf-8');
       res.status(200).send(svg);
     } else {
       const png = await QRCode.toDataURL(url, options);
       res.status(200).json({ image: png });
     }
   } catch (err) {
-    res.status(500).json({ error: 'QR generation failed', details: err });
+    res.status(500).json({ error: 'QR generation failed', details: err.message });
   }
 }
